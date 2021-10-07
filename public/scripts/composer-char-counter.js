fa0a18c57c92f3e25ab58b8a67dc12908a3e645a
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-10-07 23:54:02 +0000

    Add jQuery to enable the button and refactor comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,8 @@
+//Function to count the number of chars into the text Area
+
 $(document).ready(function () {
   $(".new-tweet form textarea").on("input", function () {
+    $(".tweet-button").prop("disabled", false);
     const textArea = this;
     const charCount = 140 - textArea.value.length;
     const counter = $(".counter");
